dc7690b4c820c586e2111737ecc8670c877ae546
kemmot <45972654+kemmot@users.noreply.github.com>
2019-11-12 21:20:41 +0000

    Initial commit of files

---
+++ bin/tasks.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+BINPATH=`dirname $0`
+python3 "$BINPATH/../tasks" "$0"
---
+++ tasks/tests/main_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class main_tests(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test(self):
+        pass
