b34f04c45abbdbc7cbdc996c0e6669bc975af208
kemmot <45972654+kemmot@users.noreply.github.com>
2019-11-22 16:58:58 +0000

    Added module file for pylint

---
+++ tasks/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+A command line task tracking utility.
+"""
+
+#from __future__ import absolute_import
+
+__author__ = 'Karl Bradshaw'
+__copyright__ = 'Copyright 2019 Karl Bradshaw'
+__license__ = 'MIT License'
+__title__ = 'tasks'
+__version__ = '0.0.1'
