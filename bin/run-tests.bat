377a3aee647ad27ae9665f90eafbacac2c22615b
kemmot <45972654+kemmot@users.noreply.github.com>
2019-11-27 18:30:19 +0000

    Added wildcard to test runner script so that it identifies all test files

--- bin/run-tests.bat
+++ bin/run-tests.bat
@@ -1,5 +1,5 @@
 pushd %~dp0..\tasks
-python -m unittest discover tests -p "_tests.py" -v
+python -m unittest discover tests -p "*_tests.py" -v
 rem coverage run -m unittest discover tests -p "*_tests.py"
 rem coverage report -m --omit="*_tests.py"
 popd
