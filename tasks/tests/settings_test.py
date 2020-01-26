42d7d6b8170acf09773aae8f2a2d76a943e1b5a2
kemmot <45972654+kemmot@users.noreply.github.com>
2020-01-26 21:10:10 +0000

    Refactored unit tests for settings class

--- tasks/tests/settings_test.py
+++ tasks/tests/settings_test.py
@@ -34,7 +34,6 @@ class SettingTests(unittest.TestCase):
     @mock.patch('settings.os.path')
     def test_read_data_location_category_not_exists(self, mock_path):
         mock_path.isfile.return_value = True
-        expected_value = '/folder'
         config = configparser.ConfigParser()
         target = settings.Settings(config=config)
         target.read('test path')
@@ -43,70 +42,43 @@ class SettingTests(unittest.TestCase):
     
     @mock.patch('settings.os.path')
     def test_read_data_location_exists(self, mock_path):
-        mock_path.isfile.return_value = True
-        expected_value = '/folder'
-        config = configparser.ConfigParser()
-        config['general'] = {}
-        config['general']['data.location'] = expected_value
-        target = settings.Settings(config=config)
-        target.read('test path')
-        result = target.data_location
-        self.assertEqual(expected_value, result)
+        self._test_setting_exists(mock_path, 'data.location', '/folder', 'data_location')
 
     @mock.patch('settings.os.path')
     def test_read_data_location_does_not_exist(self, mock_path):
-        mock_path.isfile.return_value = True
-        mock_config = mock.Mock()
-        mock_config.read = mock.MagicMock()
-        mock_config.has_option = mock.MagicMock(return_value=False)
-        s = settings.Settings(config=mock_config)
-        s.read('path')
-        with self.assertRaises(Exception):
-            data_location = s.data_location
-        mock_config.has_option.assert_called_with('general', 'data.location')
-    
-    @mock.patch('settings.os.path')
-    def test_read_data_pending_filename_not_exists(self, mock_path):
-        mock_path.isfile.return_value = True
-        expected_value = 'pending.dat'
-        config = configparser.ConfigParser()
-        config['general'] = {}
-        target = settings.Settings(config=config)
-        target.read('test path')
-        with self.assertRaises(Exception):
-            result = target.data_pending_filename
+        self._test_setting_not_exists(mock_path, 'data_location')
     
     @mock.patch('settings.os.path')
     def test_read_data_pending_filename_exists(self, mock_path):
-        mock_path.isfile.return_value = True
-        expected_value = 'pending.dat'
-        config = configparser.ConfigParser()
-        config['general'] = {}
-        config['general']['data.pending.filename'] = expected_value
-        target = settings.Settings(config=config)
-        target.read('test path')
-        result = target.data_pending_filename
-        self.assertEqual(expected_value, result)
+        self._test_setting_exists(mock_path, 'data.pending.filename', 'pending.dat', 'data_pending_filename')
+    
+    @mock.patch('settings.os.path')
+    def test_read_data_pending_filename_not_exists(self, mock_path):
+        self._test_setting_not_exists(mock_path, 'data_pending_filename')
     
     @mock.patch('settings.os.path')
     def test_read_data_done_filename_exists(self, mock_path):
-        mock_path.isfile.return_value = True
-        expected_value = 'done.dat'
-        config = configparser.ConfigParser()
-        config['general'] = {}
-        config['general']['data.done.filename'] = expected_value
-        target = settings.Settings(config=config)
-        target.read('test path')
-        result = target.data_done_filename
-        self.assertEqual(expected_value, result)
+        self._test_setting_exists(mock_path, 'data.done.filename', 'done.dat', 'data_done_filename')
     
     @mock.patch('settings.os.path')
     def test_read_data_done_filename_not_exists(self, mock_path):
+        self._test_setting_not_exists(mock_path, 'data_done_filename')
+
+    def _test_setting_not_exists(self, mock_path, test_property_name):
         mock_path.isfile.return_value = True
-        expected_value = 'done.dat'
         config = configparser.ConfigParser()
         config['general'] = {}
         target = settings.Settings(config=config)
         target.read('test path')
         with self.assertRaises(Exception):
-            result = target.data_done_filename
+            getattr(target, test_property_name)
+
+    def _test_setting_exists(self, mock_path, key, expected_value, test_property_name):
+        mock_path.isfile.return_value = True
+        config = configparser.ConfigParser()
+        config['general'] = {}
+        config['general'][key] = expected_value
+        target = settings.Settings(config=config)
+        target.read('test path')
+        result = getattr(target, test_property_name)
+        self.assertEqual(expected_value, result)
