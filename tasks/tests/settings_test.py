94da13cc2149854ab4ffc2eb61b449463c2362d3
kemmot <45972654+kemmot@users.noreply.github.com>
2020-03-10 15:24:43 +0000

    Added missing settings test for command.done.confirm

--- tasks/tests/settings_test.py
+++ tasks/tests/settings_test.py
@@ -32,6 +32,15 @@ class SettingTests(unittest.TestCase):
         mock_config.read.assert_called_with(test_path)
 
     @mock.patch('settings.os.path')
+    def test_read_command_done_confirm_exists(self, mock_path):
+        self._test_setting_exists(mock_path, \
+            'command.done.confirm', 'woble', 'command_done_confirm')
+
+    @mock.patch('settings.os.path')
+    def test_read_command_default_not_exists(self, mock_path):
+        self._test_setting_not_exists(mock_path, 'command.done.confirm')
+
+    @mock.patch('settings.os.path')
     def test_read_command_default_exists(self, mock_path):
         self._test_setting_exists(mock_path, \
             'command.default', 'woble', 'command_default')
