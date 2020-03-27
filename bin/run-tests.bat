f084e40a14c64957c47e2793bb6d206fd0c2181c
kemmot <45972654+kemmot@users.noreply.github.com>
2020-03-27 16:50:17 +0000

    Unit test coverage now working on windows

--- bin/run-tests.bat
+++ bin/run-tests.bat
@@ -1,5 +1,5 @@
 pushd %~dp0..\tasks
 python -m unittest discover tests -p "*_test.py" -v
-REM coverage run -m unittest discover tests -p "*_test.py"
-REM coverage report -m --omit="*_test.py"
+coverage run -m unittest discover tests -p "*_test.py"
+coverage report -m --omit="*_test.py"
 popd
