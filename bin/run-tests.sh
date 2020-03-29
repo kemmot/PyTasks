c600a2c1ba1af9caec9cecd6272ac3a44e917275
kemmot <45972654+kemmot@users.noreply.github.com>
2020-03-29 22:00:33 +0100

    Test script now callable from git hooks

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,8 +1,26 @@
-pushd ../tasks
+if [ $# -eq 1 ]; then
+	run_coverage=$1
+else
+	run_coverage=1
+fi
+
+#echo "count: $#"
+#echo "cover: $run_coverage"
+#exit 1
+
+dir=$(dirname "${0}")
+dir=$(realpath "$dir/../tasks")
+pushd "$dir"
+
 python3 -m unittest discover tests -p "*_test.py" -v
-if [ $? -eq 0 ]
+exit_code=$?
+
+if [ $exit_code -eq 0 ] && [ $run_coverage -eq 1 ]
 then
 	coverage run -m unittest discover tests -p "*_test.py"
 	coverage report -m --omit="*_test.py"
+	exit_code=$?
 fi
+
 popd
+exit $exit_code
