908b995d21b5d9612496fc46ef6c1be9a01bf14a
kemmot <45972654+kemmot@users.noreply.github.com>
2020-04-04 14:29:01 +0100

    FEAT-0041: pre commit hook added to check tests pass

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,12 +1,4 @@
-if [ $# -eq 1 ]; then
-	run_coverage=$1
-else
-	run_coverage=1
-fi
-
-#echo "count: $#"
-#echo "cover: $run_coverage"
-#exit 1
+#!/bin/bash
 
 dir=$(dirname "${0}")
 dir=$(realpath "$dir/../tasks")
@@ -15,7 +7,7 @@ pushd "$dir"
 python3 -m unittest discover tests -p "*_test.py" -v
 exit_code=$?
 
-if [ $exit_code -eq 0 ] && [ $run_coverage -eq 1 ]
+if [ $exit_code -eq 0 ]
 then
 	coverage run -m unittest discover tests -p "*_test.py"
 	coverage report -m --omit="*_test.py"
