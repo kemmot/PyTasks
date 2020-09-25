50db4297eaeb50fd722432bf98ff7c9712d7f8e3
kemmot <45972654+kemmot@users.noreply.github.com>
2020-09-25 15:37:01 +0100

    Addressed pylint issues in settings.py

--- tasks/settings.py
+++ tasks/settings.py
@@ -6,31 +6,31 @@ import os
 class Settings:
     def __init__(self, config=None):
         self._category = 'general'
-        if config == None:
+        if config is None:
             config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
         self._config = config
         self._logger = logging.getLogger(self.__class__.__name__)
-    
+
     @property
     def command_default(self):
         return self._get_value('command.default')
-    
+
     @property
     def command_annotate_confirm(self):
         return self._get_value('command.annotate.confirm')
-    
+
     @property
     def command_done_confirm(self):
         return self._get_value('command.done.confirm')
-    
+
     @property
     def command_modify_confirm(self):
         return self._get_value('command.modify.confirm')
-    
+
     @property
     def command_start_confirm(self):
         return self._get_value('command.start.confirm')
-    
+
     @property
     def command_stop_confirm(self):
         return self._get_value('command.stop.confirm')
@@ -42,18 +42,19 @@ class Settings:
     @property
     def data_location(self):
         return self._get_value('data.location')
-    
+
     @property
     def data_pending_filename(self):
         return self._get_value('data.pending.filename')
-    
+
     def _get_value(self, key):
         if not self._config.has_option(self._category, key):
-            raise Exception('Config element not found, category: [{}], key: [{}]'.format(self._category, key))
+            message = 'Config element not found, category: [{}], key: [{}]'
+            raise Exception(message.format(self._category, key))
         return self._config[self._category][key]
 
     def read(self, path):
-        if (not os.path.isfile(path)):
+        if not os.path.isfile(path):
             raise Exception("Config file does not exist: [{}]".format(path))
 
         try:
