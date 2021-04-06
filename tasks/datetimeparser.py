6efaa3feff65b11364cfedc3231df314a83d5fad
kemmot <45972654+kemmot@users.noreply.github.com>
2021-04-06 22:36:28 +0100

    FEAT-0080: wait attribute now supports relative dates

--- tasks/datetimeparser.py
+++ tasks/datetimeparser.py
@@ -3,10 +3,53 @@ import re
 
 
 class DateTimeParser:
+    def __init__(self, start_date=None):
+        if not start_date:
+            # NOTE: doing this as a default argument value lazy evaluates
+            self._start_date = datetime.datetime.now()
+        else:
+            self._start_date = start_date
+
+    @property
+    def start_date(self):
+        return self._start_date
+
     def parse(self, date_time_string):
-        match = re.search('\d{4}-\d{2}-\d{2}', date_time_string)
-        if match is not None:
-            date = datetime.datetime.strptime(date_time_string, '%Y-%m-%d')
+        date = self.parse_absolute_date(date_time_string)
+        if date:
+            return date
+        
+        date = self.parse_relative_date(date_time_string)
+        if date:
             return date
 
-        raise Exception('Date not recognised: [{}]'.format(date_time_string))
+        raise ValueError('Date format not recognised: [{}]'.format(date_time_string))
+
+    def parse_absolute_date(self, date_time_string):
+        match = re.search('\d{4}-\d{2}-\d{2}', date_time_string)
+        if not match:
+            return None
+
+        date = datetime.datetime.strptime(date_time_string, '%Y-%m-%d')
+        return date
+
+    def parse_relative_date(self, date_time_string):
+        regex = re.compile('(?P<direction>[+-])(?P<value>\d+)(?P<unit>[d])', re.IGNORECASE)
+        match = regex.search(date_time_string)
+        if not match:
+            return None
+        
+        direction = match.group('direction')
+        value = int(match.group('value'))
+        unit = match.group('unit')
+        if unit.upper() == 'D':
+            days = int(match.group('value'))
+            if direction == '-':
+                days *= -1
+            delta = datetime.timedelta(days=days)
+            date = self.start_date
+            date = date + delta
+            date = datetime.datetime.combine(date, datetime.datetime.min.time())
+            return date
+
+        raise ValueError('Unrecognised date unit: [{}]'.format(unit))
