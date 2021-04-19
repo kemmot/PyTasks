38602e9392bbc045e6fcfe6ed0db84241a3f8a11
kemmot <45972654+kemmot@users.noreply.github.com>
2021-04-19 20:49:14 +0100

    FEAT-0035: added support for well know columns

--- tasks/tests/entities_test.py
+++ tasks/tests/entities_test.py
@@ -46,3 +46,39 @@ class TaskTests(unittest.TestCase):
         task = entities.Task()
         task.stop()
         self.assertIsNone(task.started_time)
+
+
+class TaskAttributeNameTests(unittest.TestCase):
+    def test_get_names(self):
+        names = entities.TaskAttributeName.get_names()
+        self.assertIsNotNone(names)
+        self.assertEqual(len(names), 7)
+        self.assertEqual(names[0], entities.TaskAttributeName.DESCRIPTION)
+        self.assertEqual(names[1], entities.TaskAttributeName.END)
+        self.assertEqual(names[2], entities.TaskAttributeName.ENTRY)
+        self.assertEqual(names[3], entities.TaskAttributeName.ID)
+        self.assertEqual(names[4], entities.TaskAttributeName.START)
+        self.assertEqual(names[5], entities.TaskAttributeName.STATUS)
+        self.assertEqual(names[6], entities.TaskAttributeName.WAIT)
+
+    def test_get_name(self):
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.DESCRIPTION), 'description')
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.END), 'end')
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.ENTRY), 'entry')
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.ID), 'id')
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.START), 'start')
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.STATUS), 'status')
+        self.assertEqual(entities.TaskAttributeName.get_name(entities.TaskAttributeType.WAIT), 'wait')
+
+    def test_is_name_valid(self):
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('description'))
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('end'))
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('entry'))
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('id'))
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('start'))
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('status'))
+        self.assertTrue(entities.TaskAttributeName.is_name_valid('wait'))
+
+        self.assertFalse(entities.TaskAttributeName.is_name_valid(''))
+        self.assertFalse(entities.TaskAttributeName.is_name_valid('name'))
+        self.assertFalse(entities.TaskAttributeName.is_name_valid('womble'))
