60bd8f81bec9b473cab499fc87774471e2d2ebcd
kemmot <45972654+kemmot@users.noreply.github.com>
2021-04-23 14:31:56 +0100

    Started adding virtual environment support

---
+++ bin/build.bat
@@ -0,0 +1,5 @@
+python -m venv D:\wc\third-party\kemmot\env
+D:\wc\third-party\kemmot\env\Scripts\activate.bat
+pip install pyyaml
+pip freeze > ..\requirements.txt
+D:\wc\third-party\kemmot\env\Scripts\deactivate.bat
---
+++ bin/setup.bat
@@ -0,0 +1,4 @@
+python -m venv D:\wc\third-party\kemmot\env
+D:\wc\third-party\kemmot\env\Scripts\activate.bat
+pip install -r ../requirements.txt
+D:\wc\third-party\kemmot\env\Scripts\deactivate.bat
