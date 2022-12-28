8552eb67a481fb90c9f6c8aa345d2d6ea44c88d3
kemmot <45972654+kemmot@users.noreply.github.com>
2022-12-28 12:19:30 +0000

    Started adding context command

---
+++ tasks/commands/contextcommand.py
@@ -0,0 +1,15 @@
+
+# to create contexts
+# task context define <name> <context tags, attributes, etc>
+# e.g. task context define work +work
+# this sets a line in ini file like the following:
+# context.work = +work
+
+# to set contexts
+# task context <name>
+# e.g. task context work
+# sets the following in the ini file
+# context = work
+
+# to unset contexts
+# task context none
